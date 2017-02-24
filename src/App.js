845a1f376d44020bb4ba2042a791a4803e1c5a88
Madison Kerndt <madison.kerndt@gmail.com>
2017-02-24 00:00:20 -0700

    filter functionality

--- src/App.js
+++ src/App.js
@@ -1,30 +1,75 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { unixTimestamp } from './unixTime.js';
 import logo from './logo.svg';
 import './App.css';
 
 import User from './User'
-import userData from './mock/.mockusers';
+import Nav from './Nav'
 
-class App extends Component {
+import userData from './mock/.mockusers';
+import sortUser from './sortUser';
+
+
+function dynamicSort(property) {
+    var sortOrder = 1;
+    if(property[0] === "-") {
+        sortOrder = -1;
+        property = property.substr(1);
+    }
+    return function (a,b) {
+        var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+        return result * sortOrder;
+    };
+}
+
+export default class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      route: 0,
+      searchTerm: '',
+      filter: '',
+    };
+  }
+  componentWillUpdate(){
+    // for (var i = 0; i < userData.users.length - 1; i ++) {
+    //   this.state.filtered.push(unixTimestamp(userData.users[i].sub_end) - unixTimestamp(userData.users[i+1].id));
+    // }
+  }
+
+  handleFilterClick(button){
+    this.setState({ filter: button });
+  }
+
+  handleFilterOutput(){
+    if (this.state.filter === 'sub_end'){
+      userData.users.sort(dynamicSort("sub_end"));
+    }
+    if (this.state.filter === 'oldest'){
+      userData.users.sort(dynamicSort("created"));
+    }
+    if (this.state.filter === 'newest'){
+      userData.users.sort(dynamicSort("created").reverse());
+    } else {
+      return userData.users.map(user => <User
+        username={user.username}
+        stripe_id={user.stripe_id}
+        sub_end={unixTimestamp(user.sub_end)}
+        created={unixTimestamp(user.created)} />);
+    }
+  }
+
   render() {
     return (
       <div>
         <div>
-          { userData.users.map(user => <User user= {user}/>) }
+          <button onClick={() => this.handleFilterClick('sub_end') }>Sub End</button>
+          <button onClick={() => this.handleFilterClick('oldest') }>Oldest</button>
+          <button onClick={() => this.handleFilterClick('newest') }>New</button>
         </div>
-
-        <div className="App">
-          <div className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h2>Welcome to React</h2>
-          </div>
-          <p className="App-intro">
-            To get started, edit <code>src/App.js</code> and save to reload.
-          </p>
-        </div>
+        <div>HEY{this.state.filter}</div>
+        <div>HEY{this.handleFilterOutput()}</div>
       </div>
     );
   }
 }
-
-export default App;
--- src/User.js
+++ src/User.js
@@ -1,24 +1,15 @@
 import React, { Component } from 'react';
 
-const User = ({ user }) => {
-  if ( user.stripe_id ){
+const User = ({ username, stripe_id, sub_end, created }) => {
   return(
     <div>
-      <h1>{user.username}</h1>
-      <p>{user.stripe_id}</p>
-      <p>{user.sub_end}</p>
-    </div>
-  )
-} else {
-  return (
-    <div>
-      <h1>{user.username}</h1>
-      <p>NO STRIPE</p>
-      <p>{user.sub_end}</p>
-    </div>
+      <h1>{ username }</h1>
+      <p>{ stripe_id }</p>
+      <p>SUB END: { sub_end }</p>
+      <p >CREATED: { created }</p>
 
+    </div>
   )
-}
 }
 
 export default User;
